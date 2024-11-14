2b0955ec24159702578ddb1bf18f593514635d4e
boxanm <matej.boxan@gmail.com>
2024-11-14 09:53:01 -0500

    Check audio device status when initializing the node

--- norlab_audio_py/audio_driver.py
+++ norlab_audio_py/audio_driver.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 import sounddevice as sd
 import numpy as np
+import time
 from audio_common_msgs.msg import AudioDataStamped, AudioInfo
 
 
@@ -25,25 +26,39 @@ class AudioRecorder(Node):
         self.publisher = self.create_publisher(AudioDataStamped, "audio_stamped", 10)
         self.info_publisher = self.create_publisher(AudioInfo, "info", 10)
 
+        while True:
+            # Check if the specified device is available
+            sd._terminate()
+            sd._initialize()
+            device_names = [device['name'] for device in sd.query_devices()]
+            if any(self.device in name for name in device_names):
+                self.get_logger().info(f"Audio device '{self.device}' found.")
+                break
+            self.get_logger().warn(f"Audio device '{self.device}' not found in\n{str(device_names)}.")
+            time.sleep(1.0)
+
     def record_audio(self):
         self.get_logger().info("Recording...")
 
         # Start recording in a separate thread
         def callback(indata, frames, time, status):
             if status:
-                self.get_logger().warn(status)
+                self.get_logger().warn(f"{time}: {status}")
             self.publish_audio(indata.copy())
 
-        with sd.InputStream(
-            samplerate=self.sample_rate,
-            channels=self.channels,
-            device=self.device,
-            callback=callback,
-            dtype="float32",
-            blocksize=4096,
-        ):
-            while True:
-                rclpy.spin_once(self)  # Keep the ROS 2 node alive
+        try:
+            with sd.InputStream(
+                samplerate=self.sample_rate,
+                channels=self.channels,
+                device=self.device,
+                callback=callback,
+                dtype="float32",
+                blocksize=4096,
+            ):
+                while True:
+                    rclpy.spin_once(self)  # Keep the ROS 2 node alive
+        except ValueError as e:
+            self.get_logger().error(f"The ALSA device does not exist: {e}")
 
     def publish_audio(self, data):
         # Concatenate audio frames and convert to uint8
